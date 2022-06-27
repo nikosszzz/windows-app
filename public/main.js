b2b4f6a56868767b484a25da3714e0744cef9631
nikoszz <19nikospap@gmail.com>
2022-06-27 23:20:29 +0300

    main/electron: some refactoring
    Signed-off-by: nikoszz <19nikospap@gmail.com>

--- public/main.js
+++ public/main.js
@@ -1,12 +1,11 @@
-const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron');
-const path = require('path');
-const url = require('url');
-const Store = require('electron-store');
-const isDev = require('electron-is-dev');
+const
+    { app, BrowserWindow, ipcMain, nativeTheme } = require('electron'),
+    path = require('path'),
+    url = require('url'),
+    Store = require('electron-store'),
+    isDev = require('electron-is-dev');
 
-require('v8-compile-cache');
-require('@electron/remote/main').initialize();
-
+require('v8-compile-cache-lib').install({ cacheDir: path.join(__dirname, './cache') });
 /**
  * Initialize userStore
  * 
@@ -20,32 +19,29 @@ const userStore = new Store({
 });
 
 /**
- * Create & initialize window
+ * App initalization
  * 
  */
-
-app.on('ready', function () {
+app.on('ready', () => {
     let window;
     /**
-     * Fetch userStore
+     * Fetch user data
      */
     let storeTheme = userStore.get('themeSet');
-    let { width, height } = userStore.get('windowBounds')
-
+    let { width, height } = userStore.get('windowBounds');
     nativeTheme.themeSource = storeTheme;
 
     window = new BrowserWindow({
         autoHideMenuBar: true,
+        show: false,
         width: width,
         height: height,
-        show: false,
         webPreferences: {
             nodeIntegration: true,
-            enableRemoteModule: true,
             preload: path.join(__dirname, './appAPI.js')
         }
-    })
-    console.log(`window has been initialized`);
+    });
+    console.log(`[App] Window initializing`);
 
     window.loadURL(
         isDev
@@ -56,66 +52,68 @@ app.on('ready', function () {
                 slashes: true
             })
     );
-    window.once('ready-to-show', () => window.show());
-
     /**
-     * Open Chrome DevTools on window initialization.
-     * 
+     * Open Chrome DevTools when window is ready.
+     *
      */
-    window.webContents.openDevTools();
+    window.once('ready-to-show', () => {
+        window.show();
+        window.webContents.openDevTools();
+        console.log(`[App] Window loaded`)
+    });
 
     /**
      * Nullify the window object on close.
-     * 
+     *
      */
     window.on("closed", () => {
         window = null;
-    })
+    });
 
     /**
      * Change window dimensions in userStore on resize.
-     * 
+     *
      */
     window.on('resize', () => {
         let { width, height } = window.getBounds();
-        userStore.set('windowBounds', { width, height })
-    })
+        userStore.set('windowBounds', { width, height });
+    });
 
     /**
      * Theme controls.
-     * 
+     *
      */
     ipcMain.on('updateTheme', (event) => {
         event.reply('themeStatus', userStore.get('themeSet'));
-    })
+    });
 
     ipcMain.handle('theme:setLight', () => {
         if (nativeTheme.shouldUseDarkColors || nativeTheme.themeSource === 'system') {
             nativeTheme.themeSource = 'light';
             userStore.set('themeSet', nativeTheme.themeSource);
         }
-        return nativeTheme.shouldUseDarkColors
-    })
+        return nativeTheme.shouldUseDarkColors;
+    });
 
     ipcMain.handle('theme:setDark', () => {
         if (!nativeTheme.shouldUseDarkColors || nativeTheme.themeSource === 'system') {
             nativeTheme.themeSource = 'dark';
             userStore.set('themeSet', nativeTheme.themeSource);
         }
-        return nativeTheme.shouldUseDarkColors
-    })
+        return nativeTheme.shouldUseDarkColors;
+    });
 
     ipcMain.handle('theme:setSystem', () => {
         nativeTheme.themeSource = 'system';
         userStore.set('themeSet', nativeTheme.themeSource);
-    })
+    });
 
     /**
      * Settings API reqs.
-     * 
+     *
      */
-     ipcMain.on('userStoreReq', (event) => {
-        const windowBounds = width + `x` + height
+    ipcMain.on('userStoreReq', (event) => {
+        const windowBounds = width + `x` + height;
         event.reply('userData', userStore.get('themeSet'), windowBounds);
-    })
+    });
 })
