120d3e8725f4cfc0a0b0a72fc4b6ea0a64a9818d
nikoszz <19nikospap@gmail.com>
2022-06-27 18:30:41 +0300

    app: add base for optimization, applications pages
    Signed-off-by: nikoszz <19nikospap@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import { createRoot } from 'react-dom/client';
 import React from 'react';
 import { Route, Routes, HashRouter } from 'react-router-dom';
 import Main from './components/main';
+import Applications from './components/applications';
+import Optimizations from './components/optimizations';
 import Settings from './components/settings';
 import './styles/styles.css';
 
@@ -12,8 +14,10 @@ root.render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/settings" element={<Settings />} />
+        <Route path="/" element={<Main />}/>
+        <Route path="/applications" element={<Applications />}/>
+        <Route path="/optimizations" element={<Optimizations/>}/>
+        <Route path="/settings" element={<Settings />}/>
       </Routes>
     </HashRouter>
   </React.StrictMode>
