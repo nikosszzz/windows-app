cb6e3c9ae1ee92ff72fc366dc0edfc027f98f8a1
nikosszzz <19nikospap@gmail.com>
2022-09-08 18:00:57 +0300

    globals: make more neat implementation and merge @types
    Signed-off-by: nikosszzz <19nikospap@gmail.com>

--- src/common/globals.tsx
+++ src/common/globals.tsx
@@ -1,8 +1,27 @@
 import { versions } from "process";
 
+declare global {
+    interface Window {
+        api: any;
+        themeControl: any;
+    }
+}
+
 global.utils = {
-    version: "1.1.0",
-    electronversion: versions.electron,
+    get version(): string {
+        try {
+            return "1.1.0";
+        } catch (err) {
+            return null;
+        }
+    },
+    get electronversion(): string {
+        try {
+            return versions.electron;
+        } catch (err) {
+            return null;
+        }
+    } 
 };
 
 export default global;
